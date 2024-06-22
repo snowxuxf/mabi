fb14107eca26a2905b811c4e658d06bb2f42312b
snowxuxf <snowxuxf@126.com>
2024-06-22 11:44:33 +0000

    上传文件至 'js'

---
+++ js/私人影院.js
@@ -0,0 +1,157 @@
+var rule={
+    title:'绉佷汉褰遍櫌(琚獫鐢靛奖)',
+    // host:'https://www.bei5dy.com',
+    host:'https://www.bei5dy.net',
+    url:'/show/fyfilter/',
+    filterable:1,//鏄惁鍚敤鍒嗙被绛涢€�,
+    filter_url:'{{fl.cateId}}-{{fl.area}}-{{fl.by}}-{{fl.class}}-{{fl.lang}}-{{fl.letter}}---fypage---{{fl.year}}',
+    filter:{
+        "dianying":[{"key":"cateId","name":"绫诲瀷","value":[{"n":"鍏ㄩ儴","v":"dianying"},{"n":"鍔ㄤ綔鐗�","v":"dongzuopian"},{"n":"鍠滃墽鐗�","v":"xijupian"},{"n":"鐖辨儏鐗�","v":"aiqingpian"},{"n":"绉戝够鐗�","v":"kehuanpian"},{"n":"鎭愭€栫墖","v":"kongbupian"},{"n":"鍓ф儏鐗�","v":"juqingpian"},{"n":"鎴樹簤鐗�","v":"zhanzhengpian"},{"n":"缁煎悎鐗�","v":"lunlipian"}]},{"key":"area","name":"鍦板尯","value":[{"n":"鍏ㄩ儴","v":""},{"n":"澶ч檰","v":"澶ч檰"},{"n":"棣欐腐","v":"棣欐腐"},{"n":"鍙版咕","v":"鍙版咕"},{"n":"缇庡浗","v":"缇庡浗"},{"n":"娉曞浗","v":"娉曞浗"},{"n":"鑻卞浗","v":"鑻卞浗"},{"n":"鏃ユ湰","v":"鏃ユ湰"},{"n":"闊╁浗","v":"闊╁浗"},{"n":"寰峰浗","v":"寰峰浗"},{"n":"娉板浗","v":"娉板浗"},{"n":"鍗板害","v":"鍗板害"},{"n":"鎰忓ぇ鍒�","v":"鎰忓ぇ鍒�"},{"n":"瑗跨彮鐗�","v":"瑗跨彮鐗�"},{"n":"鍔犳嬁澶�","v":"鍔犳嬁澶�"},{"n":"鍏朵粬","v":"鍏朵粬"}]},{"key":"year","name":"骞翠唤","value":[{"n":"鍏ㄩ儴","v":""},{"n":"2023","v":"2023"},{"n":"2022","v":"2022"},{"n":"2021","v":"2021"},{"n":"2020","v":"2020"},{"n":"2019","v":"2019"},{"n":"2018","v":"2018"},{"n":"2017","v":"2017"},{"n":"2016","v":"2016"},{"n":"2015","v":"2015"},{"n":"2014","v":"2014"},{"n":"2013","v":"2013"},{"n":"2012","v":"2012"},{"n":"2011","v":"2011"},{"n":"2010~2001","v":"2010~2001"},{"n":"2000~1991","v":"2000~1991"},{"n":"1990~1960","v":"1990~1960"}]},{"key":"lang","name":"璇█","value":[{"n":"鍏ㄩ儴","v":""},{"n":"鍥借","v":"鍥借"},{"n":"鑻辫","v":"鑻辫"},{"n":"绮よ","v":"绮よ"},{"n":"闂藉崡璇�","v":"闂藉崡璇�"},{"n":"闊╄","v":"闊╄"},{"n":"鏃ヨ","v":"鏃ヨ"},{"n":"娉曡","v":"娉曡"},{"n":"寰疯","v":"寰疯"},{"n":"鍏跺畠","v":"鍏跺畠"}]},{"key":"by","name":"鎺掑簭","value":[{"n":"鏃堕棿","v":"time"},{"n":"浜烘皵","v":"hits"},{"n":"璇勫垎","v":"score"}]}],
+        "dianshiju":[{"key":"cateId","name":"绫诲瀷","value":[{"n":"鍏ㄩ儴","v":"dianshiju"},{"n":"鍥戒骇鍓�","v":"guochanju"},{"n":"娓彴鍓�","v":"gangtaiju"},{"n":"鏃ラ煩鍓�","v":"rihanju"},{"n":"娆х編鍓�","v":"oumeiju"},{"n":"鍏朵粬鍓�","v":"qitaju"}]},{"key":"area","name":"鍦板尯","value":[{"n":"鍏ㄩ儴","v":""},{"n":"澶ч檰","v":"澶ч檰"},{"n":"闊╁浗","v":"闊╁浗"},{"n":"棣欐腐","v":"棣欐腐"},{"n":"鍙版咕","v":"鍙版咕"},{"n":"鏃ユ湰","v":"鏃ユ湰"},{"n":"缇庡浗","v":"缇庡浗"},{"n":"娉板浗","v":"娉板浗"},{"n":"鑻卞浗","v":"鑻卞浗"},{"n":"鏂板姞鍧�","v":"鏂板姞鍧�"},{"n":"鍏朵粬","v":"鍏朵粬"}]},{"key":"year","name":"骞翠唤","value":[{"n":"鍏ㄩ儴","v":""},{"n":"2023","v":"2023"},{"n":"2022","v":"2022"},{"n":"2021","v":"2021"},{"n":"2020","v":"2020"},{"n":"2019","v":"2019"},{"n":"2018","v":"2018"},{"n":"2017","v":"2017"},{"n":"2016","v":"2016"},{"n":"2015","v":"2015"},{"n":"2014","v":"2014"},{"n":"2013","v":"2013"},{"n":"2012","v":"2012"},{"n":"2011","v":"2011"},{"n":"2010~2001","v":"2010~2001"},{"n":"2000~1991","v":"2000~1991"},{"n":"1990~1960","v":"1990~1960"}]},{"key":"lang","name":"璇█","value":[{"n":"鍏ㄩ儴","v":""},{"n":"鍥借","v":"鍥借"},{"n":"鑻辫","v":"鑻辫"},{"n":"绮よ","v":"绮よ"},{"n":"闂藉崡璇�","v":"闂藉崡璇�"},{"n":"闊╄","v":"闊╄"},{"n":"鏃ヨ","v":"鏃ヨ"},{"n":"鍏跺畠","v":"鍏跺畠"}]},{"key":"by","name":"鎺掑簭","value":[{"n":"鏃堕棿","v":"time"},{"n":"浜烘皵","v":"hits"},{"n":"璇勫垎","v":"score"}]}],
+        "zongyi":[{"key":"area","name":"鍦板尯","value":[{"n":"鍏ㄩ儴","v":""},{"n":"澶ч檰","v":"澶ч檰"},{"n":"娓彴","v":"娓彴"},{"n":"鏃ラ煩","v":"鏃ラ煩"},{"n":"娆х編","v":"娆х編"}]},{"key":"year","name":"骞翠唤","value":[{"n":"鍏ㄩ儴","v":""},{"n":"2023","v":"2023"},{"n":"2022","v":"2022"},{"n":"2021","v":"2021"},{"n":"2020","v":"2020"},{"n":"2019","v":"2019"},{"n":"2018","v":"2018"},{"n":"2017","v":"2017"},{"n":"2016","v":"2016"},{"n":"2015","v":"2015"},{"n":"2014","v":"2014"},{"n":"2013","v":"2013"},{"n":"2012","v":"2012"},{"n":"2011","v":"2011"},{"n":"2010~2001","v":"2010~2001"},{"n":"2000~1991","v":"2000~1991"},{"n":"1990~1960","v":"1990~1960"}]},{"key":"lang","name":"璇█","value":[{"n":"鍏ㄩ儴","v":""},{"n":"鍥借","v":"鍥借"},{"n":"鑻辫","v":"鑻辫"},{"n":"绮よ","v":"绮よ"},{"n":"闂藉崡璇�","v":"闂藉崡璇�"},{"n":"闊╄","v":"闊╄"},{"n":"鏃ヨ","v":"鏃ヨ"},{"n":"鍏跺畠","v":"鍏跺畠"}]},{"key":"by","name":"鎺掑簭","value":[{"n":"鏃堕棿","v":"time"},{"n":"浜烘皵","v":"hits"},{"n":"璇勫垎","v":"score"}]}],
+        "dongman":[{"key":"area","name":"鍦板尯","value":[{"n":"鍏ㄩ儴","v":""},{"n":"澶ч檰","v":"澶ч檰"},{"n":"鏃ユ湰","v":"鏃ユ湰"},{"n":"娆х編","v":"娆х編"},{"n":"鍏朵粬","v":"鍏朵粬"}]},{"key":"year","name":"骞翠唤","value":[{"n":"鍏ㄩ儴","v":""},{"n":"2023","v":"2023"},{"n":"2022","v":"2022"},{"n":"2021","v":"2021"},{"n":"2020","v":"2020"},{"n":"2019","v":"2019"},{"n":"2018","v":"2018"},{"n":"2017","v":"2017"},{"n":"2016","v":"2016"},{"n":"2015","v":"2015"},{"n":"2014","v":"2014"},{"n":"2013","v":"2013"},{"n":"2012","v":"2012"},{"n":"2011","v":"2011"},{"n":"2010~2001","v":"2010~2001"},{"n":"2000~1991","v":"2000~1991"},{"n":"1990~1960","v":"1990~1960"}]},{"key":"lang","name":"璇█","value":[{"n":"鍏ㄩ儴","v":""},{"n":"鍥借","v":"鍥借"},{"n":"鑻辫","v":"鑻辫"},{"n":"绮よ","v":"绮よ"},{"n":"闂藉崡璇�","v":"闂藉崡璇�"},{"n":"闊╄","v":"闊╄"},{"n":"鏃ヨ","v":"鏃ヨ"},{"n":"鍏跺畠","v":"鍏跺畠"}]},{"key":"by","name":"鎺掑簭","value":[{"n":"鏃堕棿","v":"time"},{"n":"浜烘皵","v":"hits"},{"n":"璇勫垎","v":"score"}]}]
+    },
+	filter_def:{
+		dianying:{cateId:'dianying'},
+		dianshiju:{cateId:'dianshiju'},
+		zongyi:{cateId:'zongyi'},
+		dongman:{cateId:'dongman'}
+	},
+    searchUrl:'/search/**----------fypage---/',
+    searchable:2,
+    quickSearch:0,
+    headers:{
+        'User-Agent':'MOBILE_UA'
+    },
+    timeout:5000,//缃戠珯鐨勫叏灞€璇锋眰瓒呮椂,榛樿鏄�3000姣
+    // class_parse: '.stui-header__menu&&li;a&&Text;a&&href;.*/(\\w+)/',
+    class_name:'鐢靛奖&鐢佃鍓�&鍔ㄦ极&缁艰壓',
+    class_url:'dianying&dianshiju&zongyi&dongman',
+    play_parse:true,
+	lazy:`js:
+        var html = request(input, {
+            withHeaders: true
+        });
+        let json = JSON.parse(html);
+        let setCk = Object.keys(json).find(it => it.toLowerCase() === "set-cookie");
+        let cookie = setCk ? json[setCk].split(";")[0] : "";
+        fetch_params.headers.Cookie = cookie;
+        html = JSON.parse(html).body;
+        if (/妫€娴嬩腑/.test(html)) {
+            html = request(input + "?btwaf" + html.match(/btwaf(.*?)\"/)[1], fetch_params)
+        }
+        var phtml = JSON.parse(html.match(/r player_.*?=(.*?)</)[1]);
+        var url = phtml.url;
+        if (phtml.encrypt == '1') {
+            url = unescape(url)
+        } else if (phtml.encrypt == '2') {
+            url = unescape(base64Decode(url))
+        }
+        if (/m3u8|mp4/.test(url)) {
+            input = url
+        } else {
+            input
+        }
+    `,
+    鎺ㄨ崘:'*',
+    涓€绾�:`js:
+        var d = [];
+        pdfh = jsp.pdfh;
+        pdfa = jsp.pdfa;
+        pd = jsp.pd;
+        var html = request(input, {
+            withHeaders: true
+        });
+        let json = JSON.parse(html);
+        let setCk = Object.keys(json).find(it => it.toLowerCase() === "set-cookie");
+        let cookie = setCk ? json[setCk].split(";")[0] : "";
+        fetch_params.headers.Cookie = cookie;
+        html = JSON.parse(html).body;
+        if (/妫€娴嬩腑/.test(html)) {
+            html = request(input + "?btwaf" + html.match(/btwaf(.*?)\"/)[1], fetch_params)
+        }
+        let list = pdfa(html, "ul.stui-vodlist&&li");
+        list.forEach(it => {
+            d.push({
+                title: pdfh(it, "a&&title"),
+                desc: pdfh(it, ".pic-text&&Text"),
+                pic_url: pd(it, ".lazyload&&data-original"),
+                url: pd(it, "a&&href")
+            })
+        });
+        setResult(d)
+    `,
+    浜岀骇:`js:
+        pdfh = jsp.pdfh;
+        pdfa = jsp.pdfa;
+        pd = jsp.pd;
+        VOD = {};
+        var html = request(input, {
+            withHeaders: true
+        });
+        let json = JSON.parse(html);
+        let setCk = Object.keys(json).find((it) => it.toLowerCase() === "set-cookie");
+        let cookie = setCk ? json[setCk].split(";")[0] : "";
+        fetch_params.headers.Cookie = cookie;
+        html = JSON.parse(html).body;
+        if (/妫€娴嬩腑/.test(html)) {
+            html = request(input + "?btwaf" + html.match(/btwaf(.*?)\"/)[1], fetch_params)
+        }
+        VOD.vod_name = pdfh(html, ".stui-content__detail&&h1--span&&Text");
+        VOD.type_name = pdfh(html, ".data--span:eq(0)&&Text");
+        VOD.vod_pic = pd(html, ".stui-content__thumb&&img&&data-original");
+        VOD.vod_remarks = pdfh(html, ".stui-content__thumb&&pic-text&&Text");
+        VOD.vod_actor = pdfh(html, ".data--span:eq(1)&&Text");
+        VOD.vod_director = pdfh(html, ".data--span:eq(2)&&Text");
+        VOD.vod_content = pdfh(html, ".desc.hidden-xs--a&&Text");
+        let playFrom = [];
+        let vod_tab_list = [];
+        let tabs = pdfa(html, "body .stui-pannel__head.bottom-line");
+        tabs.forEach((it) => {
+            playFrom.push(pdfh(it, "h3&&Text"))
+        });
+        for (let i = 0; i < playFrom.length; i++) {
+            let p1 = ".stui-content__playlist:eq(#id)&&li".replaceAll("#id", i);
+            let new_vod_list = [];
+            let vodList = [];
+            try {
+                vodList = pdfa(html, p1)
+            } catch (e) {}
+            for (let i = 0; i < vodList.length; i++) {
+                let it = vodList[i];
+                new_vod_list.push(pdfh(it, "body&&Text").trim() + "$" + pd(it, "a&&href"))
+            }
+            let vlist = new_vod_list.join("#");
+            vod_tab_list.push(vlist)
+        }
+        VOD.vod_play_from = playFrom.join("$$$");
+        VOD.vod_play_url = vod_tab_list.join("$$$");
+    `,
+    鎼滅储:`js:
+        var d = [];
+        pdfh = jsp.pdfh;
+        pdfa = jsp.pdfa;
+        pd = jsp.pd;
+        var html = request(input, {
+            withHeaders: true
+        });
+        let json = JSON.parse(html);
+        let setCk = Object.keys(json).find(it => it.toLowerCase() === "set-cookie");
+        let cookie = setCk ? json[setCk].split(";")[0] : "";
+        fetch_params.headers.Cookie = cookie;
+        html = JSON.parse(html).body;
+        if (/妫€娴嬩腑/.test(html)) {
+            html = request(input + "?btwaf" + html.match(/btwaf(.*?)\"/)[1], fetch_params)
+        }
+        let list = pdfa(html, ".stui-vodlist__media&&li");
+        list.forEach(it => {
+            d.push({
+                title: pdfh(it, "a&&title"),
+                desc: pdfh(it, ".pic-text&&Text"),
+                pic_url: pd(it, ".lazyload&&data-original"),
+                url: pd(it, "a&&href")
+            })
+        });
+        setResult(d)
+    `,
+}
